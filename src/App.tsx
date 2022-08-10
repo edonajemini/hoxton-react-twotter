f37a75217f16cc424d9316a05d9a8b4686ed39e1
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-08-10 21:13:04 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+<h1>twiter o</h1>
+   </div>
+  )
+}
+
+export default App
